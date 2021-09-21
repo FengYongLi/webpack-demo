9876ccd601516cbd56ecf24cd28a1785d48870f6
LiYongFeng <506076395@qq.com>
2021-09-21 23:12:37 +0800

    添加懒加载

--- src/x.js
+++ src/x.js
@@ -11,3 +11,20 @@ const div = document.getElementById('app1')
 div.innerHTML = `
   <img src="${png}">
 `
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = () => {
+  // 这里是一个异步 promise，可以使用 .then
+  const promise = import('./laz')
+  // 这里的 fn 是一个对象模块
+  promise.then((fn) => {
+    //console.log(module.default)
+    //const fn = module.default
+    //console.log(module.default)
+    console.log(fn.default)
+  }, () => {
+    console.log('模块加载失败')
+  })
+}
+
+div.appendChild(button)
